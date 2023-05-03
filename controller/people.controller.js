cbc09832f931027bb189b8672bd743139fdf1e83
quangzu <82556917+truongquangvu09@users.noreply.github.com>
2023-05-04 01:11:03 +0700

    people controller

--- controller/people.controller.js
+++ controller/people.controller.js
@@ -1,3 +1,4 @@
+const { Op } = require("sequelize");
 const { Peoples } = require("../models/index");
 
 const createpeople = async (req, res) => {
@@ -35,6 +36,58 @@ const createpeople = async (req, res) => {
   }
 };
 
+const PeopleList = async (req, res) => {
+  const { people_name } = req.query;
+  try {
+    if (people_name) {
+      const list = await Peoples.findAll({
+        where: {
+          people_name: {
+            [Op.like]: `%${people_name}%`,
+          },
+        },
+      });
+      res.status(200).send(list);
+    } else {
+      const lists = await Peoples.findAll();
+      res.status(200).send(lists);
+    }
+  } catch (error) {
+    res.status(500).send("not found");
+  }
+};
+
+const detailPeople = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const detail = await Peoples.findOne({
+      where: {
+        id,
+      },
+    });
+    res.status(200).send(detail);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+const deletePeople = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const deleted = await Peoples.destroy({
+      where: {
+        id,
+      },
+    });
+    res.status(200).send("Xóa thành công");
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
 module.exports = {
   createpeople,
+  PeopleList,
+  detailPeople,
+  deletePeople,
 };
--- router/people.router.js
+++ router/people.router.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const peopleRouter = express.Router();
-const { createpeople } = require("../controller/people.controller");
+const {
+  createpeople,
+  PeopleList,
+  detailPeople,
+  deletePeople,
+} = require("../controller/people.controller");
 const { uploadImages } = require("../middlewares/upload/upload-image");
+const { checkExist } = require("../middlewares/validations/checkExist");
+const { Peoples } = require("../models/peoples");
 
 peopleRouter.post("/people-create", uploadImages("people_image"), createpeople);
+peopleRouter.get("/people-list", PeopleList);
+peopleRouter.get("/people-detail/:id", detailPeople);
+peopleRouter.delete("/people-delete/:id", deletePeople);
 
 module.exports = {
   peopleRouter,
