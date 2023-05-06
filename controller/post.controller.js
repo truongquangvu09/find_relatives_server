60eeb681686603c3c6a9ceedc85a1dcf7c8b9ad8
quangzu <82556917+truongquangvu09@users.noreply.github.com>
2023-05-06 08:17:39 +0700

    post controller

--- controller/post.controller.js
+++ controller/post.controller.js
@@ -1,15 +1,54 @@
-const { Post } = require("../models/index");
+const { Post, Search_registrations } = require("../models/index");
 
 const createPost = async (req, res) => {
   const { searchRegistrations_id } = req.body;
   try {
     const newPost = await Post.create({ searchRegistrations_id });
-    res.status(200).send(newPost);
+    res.status(201).send(newPost);
   } catch (error) {
     res.status(500).send(error.message);
   }
 };
 
+const postList = async (req, res) => {
+  try {
+    const list = await Post.findAll({
+      include: [{ model: Search_registrations }],
+    });
+    res.status(200).send(list);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
+
+const postdetails = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const detail = await Post.findOne({
+      where: { id },
+      include: [{ model: Search_registrations }],
+    });
+    res.status(200).send(detail);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
+
+const postDelete = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const deleted = await Post.destroy({
+      where: { id },
+    });
+    res.status(200).send("Xóa thành công");
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
 module.exports = {
   createPost,
+  postList,
+  postdetails,
+  postDelete,
 };
--- router/post.router.js
+++ router/post.router.js
@@ -1,8 +1,16 @@
 const express = require("express");
-const { createPost } = require("../controller/post.controller");
+const {
+  createPost,
+  postList,
+  postdetails,
+  postDelete,
+} = require("../controller/post.controller");
 const postRouter = express.Router();
 
 postRouter.post("/post-create", createPost);
+postRouter.get("/post-list", postList);
+postRouter.get("/post-detail/:id", postdetails);
+postRouter.delete("/post-delete/:id", postDelete);
 
 module.exports = {
   postRouter,
