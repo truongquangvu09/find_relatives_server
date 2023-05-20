c001e6cfcb0c5141498d98e91998386716c00082
quangzu <82556917+truongquangvu09@users.noreply.github.com>
2023-05-21 02:00:50 +0700

    authen comment

--- router/comment.router.js
+++ router/comment.router.js
@@ -8,9 +8,10 @@ const {
 } = require("../controller/comment.controller");
 const { Comment } = require("../models/index");
 const { checkExist } = require("../middlewares/validations/checkExist");
+const { authenticate } = require("../middlewares/auth/authenticate");
 const commentRouter = express.Router();
 
-commentRouter.post("/comment-create", createComment);
+commentRouter.post("/comment-create", authenticate, createComment);
 commentRouter.get("/comment-list", commentList);
 commentRouter.get("/comment-detail/:id", checkExist(Comment), commentDetail);
 commentRouter.put("/comment-update/:id", checkExist(Comment), updateComment);
